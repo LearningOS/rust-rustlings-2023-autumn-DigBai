959838c4787f9f694872b94aa98b63ea94454a80
白继闯 <134202916+DigBai@users.noreply.github.com>
2023-10-14 11:20:04 +0000

    first commit

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -3,10 +3,10 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let x = 3;
+    let mut x = 3;//rust语言声明的赋值过是不能变的 除非mut声明  或者再声明let一次
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -3,11 +3,14 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
+    //number被赋值成字符串了
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    //不能加mut 因为不让改
+    //所以只能再生命一次let
+    let number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
